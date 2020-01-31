2d92494ab2e5bc59c4197f3ab92858f00f796e6b
Jordan Hill <jordan.hill@comcast.net>
2020-01-31 12:23:11 -0800

    Population BPIRecord Database from CSV file

---
+++ database/migrations/2020_01_31_072002_fill_b_p_i_records.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\BPIRecord;
+
+class FillBPIRecords extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //open bpi data csv
+        echo getcwd() . "\n";
+        $file = fopen("resources/csv/song_data.csv", "r");
+        $data = fgetcsv($file, ",");
+        $insert_array = array();
+
+        while (($data = fgetcsv($file, ",")) !== FALSE) 
+        {
+            $record = BPIRecord::create(array(
+                'style' => $data[0],
+                'title' => $data[1],
+                'artist' => $data[2],
+                'genre' => $data[3],
+                'nlvl' => $data[4],
+                'nnotes' => $data[5],
+                'nrecord' => $data[6],
+                'nkavg' => $data[7],
+                'hlvl' => $data[8],
+                'hnotes' => $data[9],
+                'hrecord' => $data[10],
+                'hkavg' => $data[11],
+                'alvl' => $data[12],
+                'anotes' => $data[13],
+                'arecord' => $data[14],
+                'akavg' => $data[15]
+            ));
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+
+    }
+}
